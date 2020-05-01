ce778665798451bdeabdc6cbedb4338e5e462f20
test <test@leju.com>
2020-05-01 15:18:24 +0800

    test

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -8,7 +8,7 @@ def PrintMes(content){
 def PrintMes(value,color){
 	colors = ['red'    :  "\033[40;31m >>>>>>>>>>${value}<<<<<<<<<<\003[0m",
 			  'bule'   :  "\033[47;34m >>>>>>>>>>${value}<<<<<<<<<<\003[0m",
-			  'green'  :  "[1;32m >>>>>>>>>>${value}<<<<<<<<<<[0m",
+			  'green'  :  "[1;32m >>>>>>>>>>${value}<<<<<<<<<<[m",
 			  'green1' :  "\033[40;32m >>>>>>>>>>${value}<<<<<<<<<<\003[0m"]
 	ansiColor('xterm') {
 		println(colors[color])
