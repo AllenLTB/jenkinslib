42350c6785be025406b6ffecaa086f1a83922053
test <test@leju.com>
2020-05-03 12:24:55 +0800

    test

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,9 +1,8 @@
 package org.devops
 def Build(buildType,buildShell){
     def buildTools = ["mvn":"mvn-3.6.3","gradle":"gradle-6.3","ant":"ant-1.10.7","npm":"npm-6.3"]
-    println("当前选择的构建工具是 ${buildType}")
+    tools.PrintMes("当前选择的构建工具是 ${buildType}","green1")
     buildHome = tool buildTools[buildType]
-    //sh "${buildHome}/bin/${buildType} ${buildShell}"
 	sh """
     	export PATH=${buildHome}/bin:$PATH
     	${buildType} ${buildShell}
