93a57fdaecc3b152b8debd0f3a973db72321f61a
test <test@leju.com>
2020-05-03 12:27:14 +0800

    test

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,10 +1,5 @@
 package org.devops
 
-//d打印内容
-def PrintMes(content){
-    println(content)
-}
-
 def PrintMes(value,color){
 	colors = ['red'    :  "\033[40;31m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
 			  'bule'   :  "\033[47;34m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
