3dbd490548227b2c522fd80e0091ba86008056e1
tianbao1 <tianbao1@leju.com>
2020-05-04 12:01:01 +0800

    test,tianbao1

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -3,7 +3,7 @@ package org.devops
 def PrintMes(value,color){
 	colors = ['red'    :  "\033[31m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
 			  'bule'   :  "\033[34m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
-			  'green' :  "\033[32m >>>>>>>>>>${value}<<<<<<<<<< \033[0m",
+			  'green' :  "\033[32m >>>>>>>>>>${value}<<<<<<<<<< \033[0m"]
 	ansiColor('xterm') {
 		println(colors[color])
 	}
