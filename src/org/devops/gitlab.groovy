926d3a3d63ea8d39ccec40a7723b6860b5186647
tianbao1 <tianbao1@leju.com>
2020-05-04 12:54:43 +0800

    test,tianbao1

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -3,11 +3,9 @@ package org.devops
 def gitServer = "https://gitlab-netadm.leju.com/api/v4"
 def HttpReq(reqType,reqUrl,reqBody){
 	def tools = new org.devops.tools()
-	withCredentials([string([credentialsId: 'gitlab-username_and_password', variable: 'gitlabToken')]) {
+	withCredentials([string(credentialsId: 'gitlab-username_and_password', variable: 'gitlabToken')]) {
 		result = httpRequest(
-					customHeaders: [[maskValue: true,
-					name: 'PRIVATE-TOKEN',
-					value: "${gitlabToken}"]],
+					customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]],
 					httpMode: reqType,
 					contentType: "APPLICATION_JSON",
 					consoleLogResponseBody: true,
