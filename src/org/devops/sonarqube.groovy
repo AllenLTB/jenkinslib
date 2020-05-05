69bb95fc2f71b04eb07bdd418cb29b3d937628b4
tianbao1 <tianbao1@leju.com>
2020-05-05 23:14:00 +0800

    test

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -13,7 +13,7 @@ def Scan(projectNmae,projectDesc,projectPath,projectLanguage,SonarServer,Coding,
 			-Dsonar.language="${projectLanguage}" \
         	-Dsonar.sourceEncoding="${Coding}" \
         	-Dsonar.projectVersion="${sonarData}" \
-        	-Dsonar.login="${SONAR_AUTH_TOKEN}" "${extraAgruments}"
+        	-Dsonar.login="${SONAR_AUTH_TOKEN}" ${extraAgruments}
 		"""
 	}
 }
