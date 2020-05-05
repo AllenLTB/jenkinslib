79e20f8fe5c8f258984b2031790ad31703f9cbdc
tianbao1 <tianbao1@leju.com>
2020-05-05 22:47:44 +0800

    test

---
+++ src/org/devops/trigger_generic.groovy
@@ -0,0 +1,29 @@
+package org.devops
+
+def GenTrigger(tokenValue) {
+	GenericTrigger(
+	    genericVariables: [
+	        [key: "branch", value: "\$.ref", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "buildUser", value: "\$.user_username", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "srcUrl", value: "\$.project.git_ssh_url", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "projectId", value: "\$.project.id", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "commitSha", value: "\$.checkout_sha", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "before", value: "\$.before", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "after", value: "\$.after", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "object_kind", value: "\$.object_kind", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""],
+	        [key: "mailUser", value: "\$.user_email", expressionType: "JSONPath", regexpFilter: "", defaultValue: ""]
+	    ],
+	    genericRequestVariables: [
+	        [key: "runOpts", regexpFilter: ""]
+	    ],
+	    genericHeaderVariables: [
+	    ],
+	    token: "${tokenValue}",
+	    causeString: 'Triggered on $branch',
+	    printContributedVariables: true,
+	    printPostContent: true,
+	    silentResponse: true,
+	    regexpFilterText: '$object_kind $before $after',
+	    regexpFilterExpression: '^push\\s(?!0{40}).{40}\\s(?!0{40}).{40}$'
+	)
+}
