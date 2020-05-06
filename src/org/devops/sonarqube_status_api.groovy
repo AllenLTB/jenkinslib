c2e944fe212ba23144fc7d420a76e3254fed4ddc
tianbao1 <tianbao1@leju.com>
2020-05-06 11:30:21 +0800

    test

---
+++ src/org/devops/sonarqube_status_api.groovy
@@ -0,0 +1,21 @@
+package org.devops
+
+def HttpReq(reqType,reqUrl,reqBody){
+    def sonarServer = "http://sonarqube-netadm.leju.com/api"
+    reuslt = httpRequest authentication: 'sonarqube-tianbao1',
+        httpMode: "APPLICATION_JSON",
+        consoleLogResponseBody: true,
+        ignoreSslErrors: true,
+        requestBody: reqBody,
+    	//quiet: true,
+        url: "${sonarServer}/{reqUrl}"
+    return result
+}
+
+def GetProjectStatus(projectName){
+    apiUrl = "project_branches/list?project=${projectName}"
+    response = HttpReq("GET",apiUrl,'')
+    response = readJSON text: """${response.content}"""
+    result = response["branches"][0]["status"]["qualityGateStatus"]
+    return result
+}
