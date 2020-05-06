6d0b4280e944fb5dd19a3d9e0c37be41a3adf7e4
tianbao1 <tianbao1@leju.com>
2020-05-06 21:12:40 +0800

    test

--- src/org/devops/sonarqube_api.groovy
+++ src/org/devops/sonarqube_api.groovy
@@ -59,7 +59,7 @@ def SearchQualityProfile(lang,qualityProfileName){
 	result = response["profiles"][0]
 	println(result)
 	//if (result.toString() == "${qualityProfileName}" ) {
-	if (result.toString() == null ) {
+	if (result.toString() == 'null' ) {
 		return 'true'
 	} else {
 		println("质量配置${qualityProfileName}已存在")
