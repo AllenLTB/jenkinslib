fb76440d755b942c35a4d89bec91a2a442f39b87
tianbao1 <tianbao1@leju.com>
2020-05-08 15:04:37 +0800

    tianbao1,test

--- src/org/devops/nexusapi.groovy
+++ src/org/devops/nexusapi.groovy
@@ -3,12 +3,12 @@ package org.devops
 def HttpReq(reqType,reqUrl,reqBody){
     def nexusServer = "http://10.208.3.247:8881/service/rest"
     result = httpRequest authentication: 'nexus-user-admin',
-		httpMode: reqType,
+	    httpMode: reqType,
         contentType: "APPLICATION_JSON",
         consoleLogResponseBody: true,
         ignoreSslErrors: true,
         requestBody: reqBody,
-    	//quiet: true,
+        //quiet: true,
         url: "${nexusServer}/${reqUrl}"
     return result
 }
@@ -17,5 +17,6 @@ def GetComponentsList(repository) {
 	apiUrl = "v1/components?repository=${repository}"
     response = HttpReq("GET",apiUrl,'')
 	result = readJSON text: """${response.content}"""
-	println("${result}")
+	println("${result}"[id])
+	//println("${result}")
 }
