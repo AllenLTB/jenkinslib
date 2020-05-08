d59bda7f3cc0eb07e0c5eac8f0358cfab0f88d2e
tianbao1 <tianbao1@leju.com>
2020-05-08 14:32:14 +0800

    test

---
+++ src/org/devops/nexusapi.groovy
@@ -0,0 +1,21 @@
+package org.devops
+
+def HttpReq(reqType,reqUrl,reqBody){
+    def nexusServer = "https://10.208.3.247:8881/service/rest"
+    result = httpRequest authentication: 'nexus-user-admin',
+		httpMode: reqType,
+        contentType: "APPLICATION_JSON",
+        consoleLogResponseBody: true,
+        ignoreSslErrors: true,
+        requestBody: reqBody,
+    	//quiet: true,
+        url: "${sonarServer}/${reqUrl}"
+    return result
+}
+
+def GetComponentsList(repository) {
+	apiUrl = "v1/components?repository=${repository}"
+    response = HttpReq("GET",apiUrl,'')
+	result readJSON text: """${response.content}"""
+	println("${result}")
+}
