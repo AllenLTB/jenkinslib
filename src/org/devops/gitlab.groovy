be3cc8d6d319ee2ea683833a0ad9a3ea7a914d2e
tianbao1 <tianbao1@leju.com>
2020-05-10 19:02:56 +0800

    tianbao1

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -44,7 +44,7 @@ def ListRepositoryBranch(projectId){
 	response = HttpReq('GET',apiUrl,'')
 	response = readJSON text: """${response.content}"""
 	result = response["name"]
-	println("分支ID: ${projectId}, 当前包含: ${result}"分支)
+	println("分支ID: ${projectId}, 当前包含: ${result}分支")
 	return result
 }
 
