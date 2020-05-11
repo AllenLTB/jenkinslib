a3b49ddbe66ee8e29e7752e983cb4a0eb161f857
tianbao1 <tianbao1@leju.com>
2020-05-11 17:47:56 +0800

    tianbao1

--- src/org/devops/jira.groovy
+++ src/org/devops/jira.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
-    def jiraServer = "https://10.208.3.19:8090/rest/api/2"
+    def jiraServer = "http://10.208.3.19:8090/rest/api/2"
     result = httpRequest authentication: 'jira-user-admin',
 		httpMode: reqType,
         contentType: "APPLICATION_JSON",
